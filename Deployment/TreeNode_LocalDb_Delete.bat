9ea8bd68184d9ddf4e8068b51a46c214adfc1a75
Denis Andreev <dnandreev@hotmail.com>
2018-08-18 20:19:42 +0300

    #109 Fix test db name

--- Deployment/TreeNode_LocalDb_Delete.bat
+++ Deployment/TreeNode_LocalDb_Delete.bat
@@ -1,2 +1,2 @@
-"%ProgramFiles%\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" stop "DataRoomLocalDb"
-"%ProgramFiles%\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" delete "DataRoomLocalDb"
+"%ProgramFiles%\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" stop "TreeNodeLocalDb"
+"%ProgramFiles%\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" delete "TreeNodeLocalDb"
